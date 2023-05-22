d7cbeceec1d36e70e3d50f2592ab4bffe98080d0
Sam Mennenga <smennenga19@gmail.com>
2023-05-22 14:15:17 -0500

    first commit

---
+++ prework.py
@@ -0,0 +1,6 @@
+print(hello_name("cOoLgUy420"))
+# Write a function to print “hello_USERNAME!” USERNAME is the input of the function.
+def hello_name(user_name):
+    """This is a function that prints hello_USERNAME!"""
+    return "hello_" + user_name + "!"
+print(hello_name("cOoLgUy420"))
